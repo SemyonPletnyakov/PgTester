93e0eef10ad035689805ba1e7109a6dafac030d9
Семён Семёнович Плетняков <pletnyakov01@mail.ru>
2025-03-13 21:24:45 +0300

    Added GetAverageFromStatistics method to Statistic class

--- PgTester/Models/Statistic.cs
+++ PgTester/Models/Statistic.cs
@@ -14,4 +14,18 @@ public record Statistic
 
         ClientTimeDuraction = clientTimeDuraction;
     }
+
+    public static Statistic GetAverageFromStatistics(IReadOnlyCollection<Statistic> statistics)
+    {
+        var sumClientTimeDuraction = TimeSpan.Zero;
+        
+        foreach (Statistic statistic in statistics)
+        {
+            sumClientTimeDuraction += statistic.ClientTimeDuraction;
+        }
+
+        var averageClientTimeDuraction = sumClientTimeDuraction / statistics.Count;
+
+        return new(averageClientTimeDuraction);
+    }
 }
